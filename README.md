56809f1df7fb8bb0b516649458943948e90f25a1
josesalandeando <41165516+josesalandeando@users.noreply.github.com>
2018-09-13 16:47:21 +0200

    Added README.md, refactor template and remove /tests and /testsBats (#3)
    * Run tests located tests directory if you pass parameter file to docker
    
    * Remove /testsBats directory
    
    * Refactor template
    
    * Added README.md, remove folder tests and Added run mode execution properly

---
+++ README.md
@@ -0,0 +1,81 @@
+
+# SLQA_UBUT_Framework (Framework for testing bash scripts)
+
+It's a docker image of a framework built in bash that will allow you to create unit test with a good structure and run them in a dockerized enviroment.
+
+## Introduction
+
+The Framework is oriented to the develop of tests that wants to validate scripts or functions bash in an unitary way. For it we have created two scripts with utilities for print and checking the results of test (utils-test.sh and utils-print.sh). Furthermore, 
+it is provided with a framework called Bats to print the results of the tests in a more "comfortable" way to read.  
+
+## How to Integrate QA-BASH-Framework in your project
+
+To do this we put in a Dockerfile the image of QA-BASH-Framework as shown below. With this we would have the two scripts and the framework available for your use 
+
+```bash
+FROM qa-utils-bash-unit-tests:latest
+```
+The next step is to develop our unit tests as shown in the template unit-test.sh.template
+
+If you want to print the results with Bats you also have to develop a .bats file as indicated in the template unit-test.bats.template
+
+Once our test is finished (Remember: if we want to run in "pretty" mode we will have to have two files, one .sh and another .bats). We must copy them in our new docker image to have them available when we lift the container. So we should have in our new Dockerfile two instructions how are you:
+
+```bash
+COPY ["test-example.sh",""test-example.bats"","/"]
+RUN chmod +x /test-example.sh
+```
+
+Therefore, our Dockerfile would be a file as simple as the one that follows
+
+```bash
+FROM qa-utils-bash-unit-tests:latest
+
+COPY ["test-example.sh",""test-example.bats"","/"]
+RUN chmod +x /test-example.sh
+```
+
+With this we could already run our unit tests
+
+## Run the tests from our Dockerfile created in the previous step
+
+**Create docker image from Dockerfile**
+
+Our first step will be to generate the docker image from this Dockerfile that we have created, for this:
+
+```bash
+docker build -t example-bash-unit-tests .
+```
+
+With this we would generate a docker image called example-bash-unit-tests from the Dockerfile that we have in the current route, which will be the one we have created before.
+
+**Running tests from the docker image**
+
+
+We have introduced in addition to the template, an example of use in the docker image itself (*example-unit-test.sh*, *example-unit-test.bats*). This example is available as a test of the framework and we explain how to test it in the running modes
+
+We have two run modes available:
+
+* Run with bats: To use this mode we have to run the docker container from the image created in the previous step, you pass it SCRIPT_TEST as environment variable with the name of the file to execute. We show an example below:
+
+```bash
+docker run -it -e SCRIPT_TEST=test-function.bats example-bash-unit-tests
+```
+
+If you want to run the example that we have mentioned above, run docker container  without variable SCRIPT_TEST
+
+```bash
+docker run -it example-bash-unit-tests
+```
+
+* Run with verbose mode: To use this mode we have to run the docker container from the image created in the previous step, you pass it SCRIPT_TEST as environment variable with the name of the file to execute and variable MODE_TESTS with value "VERBOSE". We show an example below:
+
+```bash
+docker run -it -e MODE_TESTS=VERBOSE -e SCRIPT_TEST=test-function.bats example-bash-unit-tests
+```
+
+If you want to run the example that we have mentioned above, run docker container  without variable SCRIPT_TEST
+
+```bash
+docker run -it -e MODE_TESTS=VERBOSE example-bash-unit-tests
+```
--- example/example-unit-test.sh
+++ example/example-unit-test.sh
@@ -35,6 +35,26 @@ function printResultTestAddNumbers {
 
 }
 
+function runTestAddNumbers {
+
+#   *************** PRINTS INPUT AND HEADER TEST **********************
+    printHeaderTestCaseAndCallFunction "$numTestCase" "$FUNCTION_NAME" "${INPUT_VARIABLES_FUNCTION[@]}"
+#   *************** EXECUTE FUNCTION **********************
+    if [ "$RUN_OPTIONS" = "ERROR_MODE" ]; then
+        local EXECUTION_ERROR=$(echo $(addNumbers "$NUMBER_ONE" "$NUMBER_TWO" "RESULT" "$FILE"))
+    else
+        addNumbers "$NUMBER_ONE" "$NUMBER_TWO" "RESULT" "$FILE"
+    fi
+#   *************** PRINTS RESULT TEST **********************
+    printFooterCallFunction "$FUNCTION_NAME"
+    printResultTestAddNumbers
+    printFooterResultTestCase "$FUNCTION_NAME"
+
+#   *************** INCREMENTS NUM TEST CASE **********************
+    numTestCase=$(($numTestCase+1))
+}
+
+
 ERROR_UNIT_TEST_FUNCTION=0
 FUNCTION_NAME="addNumbers"
 #   *************** FUNCTION INPUT GLOBAL FOR ALL TEST **********************
@@ -46,6 +66,10 @@ NUMBER_ONE=""
 NUMBER_TWO=""
 RESULT=""
 INPUT_VARIABLES_FUNCTION=("NUMBER_ONE" "NUMBER_TWO" "RESULT")
+#   *************** VARS NEEDED FOR PRINT RESULTS **********************
+RESULT_EXPECTED=""
+ERROR_EXPECTED=""
+RUN_OPTIONS=""
 #   *************** TEST CASE COUNT **********************
 let numTestCase=1
 
@@ -62,19 +86,15 @@ function testAddNumbers {
     NUMBER_ONE="174"
     NUMBER_TWO="75"
     RESULT=""
-#   *************** PRINTS INPUT AND HEADER TEST **********************
-    printHeaderTestCaseAndCallFunction "$numTestCase" "$FUNCTION_NAME" "${INPUT_VARIABLES_FUNCTION[@]}"
-#   *************** EXECUTE FUNCTION **********************
-    addNumbers "$NUMBER_ONE" "$NUMBER_TWO" "RESULT" "$FILE"
+
+#   *************** VARS NEEDED FOR PRINT RESULTS **********************
     RESULT_EXPECTED=249
     ERROR_EXPECTED="N"
-#   *************** PRINTS RESULT TEST **********************
-    printFooterCallFunction "$FUNCTION_NAME"
-    printResultTestAddNumbers
-    printFooterResultTestCase "$FUNCTION_NAME"
+    RUN_OPTIONS=""
 
-#   *************** INCREMENTS NUM TEST CASE **********************
-    numTestCase=$(($numTestCase+1))
+#   *************** RUN TEST FUNCTION **********************
+
+    runTestAddNumbers
 
 
 #    TEST CASE 2 ERROR PATH: The first parameter is a letter
@@ -82,31 +102,22 @@ function testAddNumbers {
     NUMBER_ONE="a"
     NUMBER_TWO="321"
     RESULT=""
-    printHeaderTestCaseAndCallFunction "$numTestCase" "$FUNCTION_NAME" "${INPUT_VARIABLES_FUNCTION[@]}"
-#   This is by the use of bats, when a function exit it has problems and they are solved this
-    local EXECUTION_ERROR=$(echo $(addNumbers "$NUMBER_ONE" "$NUMBER_TWO" "RESULT" "$FILE"))
     RESULT_EXPECTED=
     ERROR_EXPECTED="Y"
-    printFooterCallFunction "$FUNCTION_NAME"
-    printResultTestAddNumbers
-    printFooterResultTestCase "$FUNCTION_NAME"
-    numTestCase=$(($numTestCase+1))
+    RUN_OPTIONS="ERROR_MODE"
 
+    runTestAddNumbers
 
 #    TEST CASE 3 ERROR PATH: The second parameter is a letter
 
     NUMBER_ONE="293"
     NUMBER_TWO="b"
     RESULT=""
-    printHeaderTestCaseAndCallFunction "$numTestCase" "$FUNCTION_NAME" "${INPUT_VARIABLES_FUNCTION[@]}"
-#    addNumbers "$NUMBER_ONE" "$NUMBER_TWO" "RESULT" "$FILE"
-    EXECUTION_ERROR=$(echo $(addNumbers "$NUMBER_ONE" "$NUMBER_TWO" "RESULT" "$FILE"))
     RESULT_EXPECTED=
     ERROR_EXPECTED="Y"
-    printFooterCallFunction "$FUNCTION_NAME"
-    printResultTestAddNumbers
-    printFooterResultTestCase "$FUNCTION_NAME"
-    numTestCase=$(($numTestCase+1))
+    RUN_OPTIONS="ERROR_MODE"
+
+    runTestAddNumbers
 
 
 #    TEST CASE 4 ERROR PATH: Two parameters are numbers and NOT exist file
@@ -114,15 +125,11 @@ function testAddNumbers {
     NUMBER_ONE="157"
     NUMBER_TWO="326"
     RESULT=""
-    printHeaderTestCaseAndCallFunction "$numTestCase" "$FUNCTION_NAME" "${INPUT_VARIABLES_FUNCTION[@]}"
-#    addNumbers "$NUMBER_ONE" "$NUMBER_TWO" "RESULT" "A_File_that_NOT_exists"
-    EXECUTION_ERROR=$(echo $(addNumbers "$NUMBER_ONE" "$NUMBER_TWO" "RESULT" "A_File_that_NOT_exists"))
     RESULT_EXPECTED=
     ERROR_EXPECTED="Y"
-    printFooterCallFunction "$FUNCTION_NAME"
-    printResultTestAddNumbers
-    printFooterResultTestCase "$FUNCTION_NAME"
-    numTestCase=$(($numTestCase+1))
+    RUN_OPTIONS="ERROR_MODE"
+
+    runTestAddNumbers
 
 
 #   *************** END TEST CASES **********************
